988065a938d33295b6ddf58ff6a3781bd70ea8b3
Nayed <nayed.saidali@gmail.com>
2015-08-23 01:39:26 +0200

    add routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,36 @@
 |
 */
 
-$app->get('/', function () use ($app) {
-    return $app->welcome();
+use App\Models\Quote;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+/**
+ *  Display the quote of the day
+ */
+$app->get('/', function() use ($app) {
+    /*
+     *  Picks a different quote every day
+     *  (for a maximum of 366 quotes)
+     *      - $count: the total number of available quotes
+            - $day: the current day of the year (from 0 to 365)
+            - $page: the page to look for to retrieve the correct record
+     */
+    $count = Quote::query()->get()->count();
+    $day = (int) date('z');
+    $page = $day % $count + 1;
+
+    $quotes = Quote::query()->get()->forPage($page, 1)->all();
+
+    if (empty($quotes))
+        throw new ModelNotFoundException();
+
+    return view('quote', ['quote' => $quotes[0]]);
+});
+
+/**
+ *  Dispplay a specific quote
+ */
+$app->get('/{id', function($id) use ($app) {
+    $quote = Quote::query()->findOrFail($id);
+    return view('quote', compact('quote'));
 });
