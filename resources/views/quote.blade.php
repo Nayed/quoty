8f6fd94d5cd4d9b6ab54d2b3ba808ad4135af270
Nayed <nayed.saidali@gmail.com>
2015-08-25 23:50:15 +0200

    tweaking

--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -4,11 +4,11 @@
     <link href="/css/style.css" rel="stylesheet" type="text/css"/>
     <link href='http://fonts.googleapis.com/css?family=Alegreya:400,700|Roboto+Condensed' rel='stylesheet' type='text/css'>
 </head>  
-<body style="background-image: url('/img/{{$quote->background}}')">  
+<body style="background-image: url('/img/{{ $quote->background }}')">  
 <div class="container">  
     <div class="quote-container">
-        <p class="text">{{$quote->text}}</p>
-        <p class="author">— {{$quote->author}}</p>
+        <p class="text">{{ $quote->text }}</p>
+        <p class="author">— {{ $quote->author }}</p>
     </div>
 </div>  
 </body>  
